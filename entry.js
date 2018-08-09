621d9a795334aab1881c71d2df5e8b93a3066036
Danny Guo <dannyguo91@gmail.com>
2018-08-09 11:50:30 -0400

    Use the storage-available package

--- entry.js
+++ entry.js
@@ -2,6 +2,7 @@ import 'whatwg-fetch';
 import 'autotrack/lib/plugins/outbound-link-tracker';
 import debug from 'debug';
 import shuffle from 'lodash.shuffle';
+import storageAvailable from 'storage-available';
 
 import './styles.scss';
 
@@ -49,33 +50,6 @@ const WHITELIST = [
     'time',
     'usa-today'
 ];
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Feature-detecting_localStorage
-function storageAvailable(type) {
-    try {
-        var storage = window[type];
-        const x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-    } catch (e) {
-        return (
-            e instanceof DOMException &&
-            // everything except Firefox
-            (e.code === 22 ||
-                // Firefox
-                e.code === 1014 ||
-                // test name field too, because code might not be present
-                // everything except Firefox
-                e.name === 'QuotaExceededError' ||
-                // Firefox
-                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-            // acknowledge QuotaExceededError only if there's something
-            // already stored
-            storage.length !== 0
-        );
-    }
-}
 
 // Delete viewed links that are old to avoid hitting the localStorage limit.
 function pruneViewedLinks() {
